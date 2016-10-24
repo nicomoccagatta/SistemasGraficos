fe91504b42531ce580f529123c5487c515b601b0
nicomoccagatta <nicomoccagatta@gmail.com>
2016-10-24 17:30:58 -0300

    Curva de terreno FUNCIONANDO

--- js/Field.js
+++ js/Field.js
@@ -47,23 +47,34 @@ function Field(from_x, to_x, from_y, to_y, diameter, min_height, max_height, poi
     }
     
  
-    this.Base1 = function(u) {
-        return -u*u+u+0.5;
+    this.Base2 = function(u) {
+        return 0.5*u*u;
     }
     
 
     this.get_center_xy = function(num_section,u) {
         // repeat the first and last items
 
-        var points_aux = this.mapped_points[0];
-        points_aux += this.mapped_points;
-        points_aux += this.mapped_points[this.mapped_points.length-1];
+        var points_aux = [];
+        points_aux[0] = this.mapped_points[0];
+        for (var i=0; i<this.mapped_points.length; i++) {
+            points_aux[i+1] = this.mapped_points[i];
+        }
+        points_aux[points_aux.length] = this.mapped_points[this.mapped_points.length-1];
 
-        var aux =
-        this.Base0(u) * points_aux[0+num_section] +      // b0*p0
-        this.Base1(u) * points_aux[1+num_section] +      // b1*p1
-        this.Base2(u) * points_aux[2+num_section];       // b2*p2
+        var aux = [];
+        aux[0] =
+        this.Base0(u) * points_aux[0+num_section][0] +      // b0*p0
+        this.Base1(u) * points_aux[1+num_section][0] +      // b1*p1
+        this.Base2(u) * points_aux[2+num_section][0];       // b2*p2
+        aux[1] =
+        this.Base0(u) * points_aux[0+num_section][1] +      // b0*p0
+        this.Base1(u) * points_aux[1+num_section][1] +      // b1*p1
+        this.Base2(u) * points_aux[2+num_section][1];       // b2*p2
 
+        console.log("seccion: "+num_section);
+        console.log("u: "+u);
+        console.log("centro: "+ aux[0]+","+aux[1]+".");
         return aux;
     }
 
@@ -76,27 +87,27 @@ function Field(from_x, to_x, from_y, to_y, diameter, min_height, max_height, poi
         var min_center_y = from_y + radius;
         var delta_y = max_center_y - min_center_y;
 
-        console.log("Y va de :"+min_center_y+", a: "+max_center_y+".");
+        //console.log("Y va de :"+min_center_y+", a: "+max_center_y+".");
 
         var max_x = to_x;
         var min_x = from_x;
         var delta_x = max_x - min_x;
-        console.log("X va de :"+min_x+", a: "+max_x+".");
+        //console.log("X va de :"+min_x+", a: "+max_x+".");
 
         for (var i=0; i < points.length; i++) {
-            console.log("Points ["+i+"]: " + points[i][1] + "," + points[i][0]+"....");
+            //console.log("Points ["+i+"]: " + points[i][1] + "," + points[i][0]+"....");
         }
 
         var formatted_points = [];
         for (var i=0; i<points.length; i++) {
             formatted_points[i] = [1.0 - points[i][1]/height_viewport , 1.0 - points[i][0]/width_viewport];
-            console.log("Formatted Points ["+i+"]: " + formatted_points[i][0] + "," + formatted_points[i][1]+"....");
+           // console.log("Formatted Points ["+i+"]: " + formatted_points[i][0] + "," + formatted_points[i][1]+"....");
         }
 
 
         for (var i=0; i<formatted_points.length; i++) {
             this.mapped_points[i] = [min_x + formatted_points[i][0] * delta_x , min_center_y + formatted_points[i][1] * delta_y];
-            console.log("Mapped Points ["+i+"]: " + this.mapped_points[i][0] + "," + this.mapped_points[i][1]+"....");
+           // console.log("Mapped Points ["+i+"]: " + this.mapped_points[i][0] + "," + this.mapped_points[i][1]+"....");
         }
 
         this.mapped_points.sort(function(a, b){return a[0]-b[0]});
@@ -105,16 +116,21 @@ function Field(from_x, to_x, from_y, to_y, diameter, min_height, max_height, poi
         }
 
         var quant_sections = points.length;
-
         var index_index_buffer = 0;
-        for (var num_section = 0; i < quant_sections; i++) {
-            for (var dif_u = 0.0; dif_u <= 0.8 ; dif_u += 0.20) {
+        var index_upper_land = 0;
+        var index_lower_land = 0;
+
+
+        for (var num_section = 0; num_section < quant_sections; num_section++) {
+            for (var dif_u = 0.0; dif_u <= 0.8 ; dif_u += 0.2) {
 
                 var aux_center_arc = this.get_center_xy(num_section, dif_u);
                 var next_center_arc = this.get_center_xy(num_section, dif_u+0.20);
 
                 var top_arc_y = aux_center_arc[1] + radius;
+                var next_top_arc_y = next_center_arc[1] + radius;
                 var bottom_arc_y = aux_center_arc[1] - radius;
+                var next_bottom_arc_y = next_center_arc[1] - radius;
 
 
                 //////////////////////////////
@@ -136,16 +152,34 @@ function Field(from_x, to_x, from_y, to_y, diameter, min_height, max_height, poi
                     var z = max_height + (height * sinTheta);
                         
                     this.fillBuffers(this.normal_buffer, this.position_buffer, this.color_buffer, x, y, z, r, g, b);
+                    //PARTENUEVA
+                    this.index_buffer.push(index_index_buffer);
+                    index_index_buffer++;
+                    ////////////
                     
                     x = next_center_arc[0];
+                    y = next_center_arc[1] - (radius * cosTheta);
                     this.fillBuffers(this.normal_buffer, this.position_buffer, this.color_buffer, x, y, z, r, g, b);
+                    //PARTENUEVA
+                    this.index_buffer.push(index_index_buffer);
+                    index_index_buffer++;
+                    ///////
                 }
+                    //PARTENUEVA
+                    index_index_buffer--;
+                    this.index_buffer.push(index_index_buffer);
+                    this.index_buffer.push(index_index_buffer - 362);
+                    index_index_buffer++;
+                    ///////
 
-
+/*
                 for (var i = 0; i < 363; i++) {
                     this.index_buffer.push(index_index_buffer);
                     index_index_buffer++;
                 }
+                    this.index_buffer.push(index_index_buffer-1);
+
+*/
 
                 ///////////////////////////
                 // END ARC OF THE FIELD
@@ -163,20 +197,27 @@ function Field(from_x, to_x, from_y, to_y, diameter, min_height, max_height, poi
                 var y = to_y;
                 var z = max_height;
                 this.fillBuffers(this.normal_buffer_upper_land, this.position_buffer_upper_land, this.color_buffer_upper_land, x, y, z, r, g, b);
-                this.index_buffer_upper_land.push(0);
+                this.index_buffer_upper_land.push(index_upper_land);
+                index_upper_land++;
 
                 x = next_center_arc[0];
                 this.fillBuffers(this.normal_buffer_upper_land, this.position_buffer_upper_land, this.color_buffer_upper_land, x, y, z, r, g, b);
-                this.index_buffer_upper_land.push(1);
+                this.index_buffer_upper_land.push(index_upper_land);
+                index_upper_land++;
 
                 x = aux_center_arc[0];
                 y = top_arc_y;
                 this.fillBuffers(this.normal_buffer_upper_land, this.position_buffer_upper_land, this.color_buffer_upper_land, x, y, z, r, g, b);
-                this.index_buffer_upper_land.push(2);
+                this.index_buffer_upper_land.push(index_upper_land);
+                index_upper_land++;
 
                 x = next_center_arc[0];
+                y = next_top_arc_y;
                 this.fillBuffers(this.normal_buffer_upper_land, this.position_buffer_upper_land, this.color_buffer_upper_land, x, y, z, r, g, b);
-                this.index_buffer_upper_land.push(3);
+                this.index_buffer_upper_land.push(index_upper_land);
+                this.index_buffer_upper_land.push(index_upper_land);
+                this.index_buffer_upper_land.push(index_upper_land-2);
+                index_upper_land++;
 
                 ///////////////////////////////////
                 //END UPPER LAND
@@ -194,21 +235,28 @@ function Field(from_x, to_x, from_y, to_y, diameter, min_height, max_height, poi
                 y = bottom_arc_y;
                 z = max_height;
                 this.fillBuffers(this.normal_buffer_lower_land, this.position_buffer_lower_land, this.color_buffer_lower_land, x, y, z, r, g, b);
-                this.index_buffer_lower_land.push(0);
+                this.index_buffer_lower_land.push(index_lower_land);
+                index_lower_land++;
 
 
                 x = next_center_arc[0];
+                y = next_bottom_arc_y;
                 this.fillBuffers(this.normal_buffer_lower_land, this.position_buffer_lower_land, this.color_buffer_lower_land, x, y, z, r, g, b);
-                this.index_buffer_lower_land.push(1);
+                this.index_buffer_lower_land.push(index_lower_land);
+                index_lower_land++;
 
                 x = aux_center_arc[0];
                 y = from_y;
                 this.fillBuffers(this.normal_buffer_lower_land, this.position_buffer_lower_land, this.color_buffer_lower_land, x, y, z, r, g, b);
-                this.index_buffer_lower_land.push(2);
+                this.index_buffer_lower_land.push(index_lower_land);
+                index_lower_land++;
 
                 x = next_center_arc[0];
                 this.fillBuffers(this.normal_buffer_lower_land, this.position_buffer_lower_land, this.color_buffer_lower_land, x, y, z, r, g, b);
-                this.index_buffer_lower_land.push(3);
+                this.index_buffer_lower_land.push(index_lower_land);
+                this.index_buffer_lower_land.push(index_lower_land);
+                this.index_buffer_lower_land.push(index_lower_land-2);
+                index_lower_land++;
 
                 ///////////////////////////////////
                 // END LOWER LAND
